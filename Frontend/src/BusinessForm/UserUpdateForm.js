a6635466d88cdd86fe0a35ba90d04df34f0f472c
AKhil <akhilacharya343@gmail.com>
2024-11-26 18:26:15 +0530

    final

--- Frontend/src/BusinessForm/UserUpdateForm.js
+++ Frontend/src/BusinessForm/UserUpdateForm.js
@@ -1,9 +1,7 @@
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; 
 import toast from 'react-hot-toast';
-
 const UserUpdateForm = () => {
   const [formData, setFormData] = useState({
     name: '',
