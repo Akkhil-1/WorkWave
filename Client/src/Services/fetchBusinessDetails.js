db22d70897243497bc03f2c8b4f5f11388994e52
AKhil <akhilacharya343@gmail.com>
2024-12-13 22:19:33 +0530

    final

--- Client/src/Services/fetchBusinessDetails.js
+++ Client/src/Services/fetchBusinessDetails.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001" // Replace with your actual API base URL
+const API_BASE_URL = "https://workwave-aage.onrender.com" // Replace with your actual API base URL
 
 export const fetchBusinessDetails = async (id) => { 
   try {
--- Client/src/Services/fetchServiceData.js
+++ Client/src/Services/fetchServiceData.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001";
+const API_BASE_URL = "https://workwave-aage.onrender.com";
 
 export const fetchServiceDetails = async (id) => {
   try {
