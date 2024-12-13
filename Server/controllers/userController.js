0f04f4e19a30b31f8aab3f4176f1652b1917ae55
Shivansh Khunger <khunger.shivansh@outlook.com>
2024-12-13 22:35:31 +0530

    add res.cookie in user

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -90,12 +90,19 @@ const login = async (req, res) => {
     );
     console.log(token);
     res.cookie("__vercel_live_token", token, {
-      httpOnly: true,   // Make the cookie accessible only via HTTP(S), not JavaScript
+      httpOnly: true, // Make the cookie accessible only via HTTP(S), not JavaScript
       maxAge: 60 * 60 * 1000, // Set the cookie expiration time (1 hour)
       sameSite: "None", // Allow the cookie to be sent in cross-site requests
-      secure: true,     // Ensure the cookie is only sent over HTTPS
+      secure: true, // Ensure the cookie is only sent over HTTPS
     });
-  
+
+    res.cookie("token", token, {
+      httpOnly: true, // Make the cookie accessible only via HTTP(S), not JavaScript
+      maxAge: 60 * 60 * 1000, // Set the cookie expiration time (1 hour)
+      sameSite: "None", // Allow the cookie to be sent in cross-site requests
+      secure: true, // Ensure the cookie is only sent over HTTPS
+    });
+
     console.log("Login successful, returning token");
     return res.status(200).json({
       token,
