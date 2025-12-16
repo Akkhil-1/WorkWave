8af3d53f90d8ee849a49db429764c678450696db
Akkhil-1 <akhilacharya343@gmail.com>
2025-12-16 15:00:36 +0530

    correct review section

--- Client/src/Services/fetchServiceData.js
+++ Client/src/Services/fetchServiceData.js
@@ -4,16 +4,13 @@ const API_BASE_URL = "https://workwave-aage.onrender.com";
 
 export const fetchServiceDetails = async (id) => {
   try {
-    console.log("Frontend: Fetching details for id:", id);
     const response = await axios.get(`${API_BASE_URL}/services/${id}`);
     return response.data;
   } catch (error) {
     if (error.response?.status === 404) {
-      console.warn("Service not found, continuing app flow");
       return null;
     }
-
-    console.error("Unexpected error fetching service details:", error);
+    console.error("Failed to fetch service details:", error);
     return null;
   }
 };
